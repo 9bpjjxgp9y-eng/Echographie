cc2e400e6753758d02cc01bff9411bc2e2f686a4
Margot theisen <9bpjjxgp9y@privaterelay.appleid.com>
2026-04-30 10:14:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
 let score = 0;
+let questionIndex = 0;
+
+const questions = [
+  {
+    q: "Was nutzt Echographie?",
+    a: "ultraschall"
+  },
+  {
+    q: "Was wird bei der Echographie erzeugt?",
+    a: "bilder"
+  },
+  {
+    q: "Welche Wellen werden gesendet?",
+    a: "schallwellen"
+  }
+];
 
 function showInfo() {
   document.getElementById("output").innerText =
-  "Echographie nutzt Ultraschallwellen, um Bilder im Körper zu erzeugen.";
+    "Echographie nutzt Ultraschallwellen, die im Körper reflektiert werden.";
 }
 
 function quiz() {
-  let answer = prompt("Was nutzt Echographie?");
-
-  if (answer && answer.toLowerCase() === "ultraschall") {
-    score++;
-    alert("Richtig! Punktestand: " + score);
-  } else {
-    alert("Falsch! Richtige Antwort: Ultraschall. Punktestand: " + score);
+  if (questionIndex >= questions.length) {
+    alert("Quiz fertig! Punkte: " + score + "/" + questions.length);
+    score = 0;
+    questionIndex = 0;
+    return;
   }
+
+  let userAnswer = prompt(questions[questionIndex].q);
+
+  if (userAnswer && userAnswer.toLowerCase() === questions[questionIndex].a) {
+    score++;
+    alert("Richtig!");
+  } else {
+    alert("Falsch! Richtige Antwort: " + questions[questionIndex].a);
+  }
+
+  questionIndex++;
 }
