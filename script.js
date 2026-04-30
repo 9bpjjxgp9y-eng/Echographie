d7c8664a329741d658307a8820d496fa51d40b29
Margot theisen <9bpjjxgp9y@privaterelay.appleid.com>
2026-04-30 13:30:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -125,3 +125,89 @@ function restartQuiz() {
 
 updateUI();
 goHome();
+const theoryContent = [
+  {
+    id: "intro",
+    de: "00 Einführung",
+    fr: "00 Introduction"
+  },
+  {
+    id: "base1",
+    de: "01 Grundlagen: Ultraschall",
+    fr: "01 Principes: Ultrasons"
+  },
+  {
+    id: "base2",
+    de: "02 Kompression / Rarefaktion",
+    fr: "02 Compression / Rarefaction"
+  },
+  {
+    id: "base3",
+    de: "03 Impedanz & Gewebe",
+    fr: "03 Impédance & tissus"
+  },
+  {
+    id: "base4",
+    de: "04 Messprinzip",
+    fr: "04 Principe de mesure"
+  },
+  {
+    id: "types1",
+    de: "05 Echokardiographie",
+    fr: "05 Échocardiographie"
+  },
+  {
+    id: "types2",
+    de: "06 Schwangerschaft",
+    fr: "06 Grossesse"
+  },
+  {
+    id: "types3",
+    de: "07 Abdomen",
+    fr: "07 Abdominale"
+  },
+  {
+    id: "types4",
+    de: "08 2D / 3D / 4D / Doppler",
+    fr: "08 2D / 3D / 4D / Doppler"
+  },
+  {
+    id: "conclusion",
+    de: "09 Fazit",
+    fr: "09 Conclusion"
+  }
+];
+
+function showTheory() {
+  resetScreens();
+  document.getElementById("theoryScreen").style.display = "block";
+
+  let menuHTML = "";
+
+  theoryContent.forEach(item => {
+    let title = item[lang];
+
+    menuHTML += `
+      <button onclick="openDetail('${item.id}')">${title}</button><br>
+    `;
+  });
+
+  document.getElementById("theoryMenu").innerHTML = menuHTML;
+}
+
+function openDetail(id) {
+  resetScreens();
+  document.getElementById("theoryDetailScreen").style.display = "block";
+
+  let item = theoryContent.find(x => x.id === id);
+
+  document.getElementById("detailTitle").innerText = item[lang];
+
+  // 👉 HIER KOMMT DEIN TEXT SPÄTER
+  document.getElementById("detailContent").innerText =
+    "Hier kommt dein Inhalt...";
+}
+
+function backToTheory() {
+  showTheory();
+}
