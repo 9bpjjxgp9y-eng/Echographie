2fc31d90798bacd3e1a26c957bf7099ae0e519a2
Margot theisen <9bpjjxgp9y@privaterelay.appleid.com>
2026-04-30 08:41:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ function showInfo() {
   document.getElementById("output").innerText =
   "Echographie nutzt Ultraschallwellen.";
 } 
+function quiz() {
+  let answer = prompt("Was nutzt Echographie?");
+  
+  if (answer === "Ultraschall") {
+    alert("Richtig!");
+  } else {
+    alert("Falsch! Die richtige Antwort ist Ultraschall.");
+  }
+}
